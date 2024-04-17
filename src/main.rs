d719399aca08d42eeac2f90cb2fd49372366977d
Mathis EON <eon@abes.fr>
2024-04-17 21:53:54 +0200

    wip tui

--- src/main.rs
+++ src/main.rs
@@ -26,19 +26,18 @@ use cli::Cli;
 mod tui;
 use tui::*;
 
-use tabled::Table;
+use std::{error::Error, io};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+use ratatui::prelude::*;
 
 mod parser;
 use crate::parser::parse_query;
-
-use tabled::settings::object::Rows;
-use tabled::settings::Style;
-use tabled::settings::{measurement::Percent, Width};
-
-use tabled::settings::{
-    peaker::{PriorityMax, PriorityMin},
-    Settings, Padding,
-};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     pyo3::append_to_inittab!(q3);
@@ -50,29 +49,52 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     queries.expand()?;
 
-    let mut table_data: Vec<TableRow> = queries.into();
+    let table_data: Vec<TableRow> = queries.into();
+    //
+    // let mut table = if let Some(id) = args.get {
+    //     table_data = table_data
+    //         .into_iter()
+    //         .filter(|elem| elem.id == id)
+    //         .collect();
+    //     Table::new(table_data)
+    // } else {
+    //     Table::new(table_data)
+    // };
+    //
+    // let settings = Settings::new(
+    //     Width::increase(40).priority::<PriorityMin>(),
+    //     Width::wrap(Percent(70)).priority::<PriorityMax>(),
+    // );
+    //
+    // table
+    //     .with(settings)
+    //     .modify(Rows::new(1..), Padding::new(0, 0, 0, 1))
+    //     .with(Style::rounded());
 
-    let mut table = if let Some(id) = args.get {
-        table_data = table_data
-            .into_iter()
-            .filter(|elem| elem.id == id)
-            .collect();
-        Table::new(table_data)
-    } else {
-        Table::new(table_data)
-    };
+    // println!("{}", table.to_string());
 
-    let settings = Settings::new(
-        Width::increase(40).priority::<PriorityMin>(),
-        Width::wrap(Percent(70)).priority::<PriorityMax>(),
-    );
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    table
-        .with(settings)
-        .modify(Rows::new(1..), Padding::new(0, 0, 0, 1))
-        .with(Style::rounded());
+    // create app and run it
+    let app = App::new(table_data);
+    let res = run_app(&mut terminal, app);
 
-    println!("{}", table.to_string());
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{err:?}");
+    }
 
     Ok(())
 }
--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,13 +1,19 @@
 use crate::QStore;
+use arboard::Clipboard;
 
-use tabled::{Table, Tabled};
+use std::{error::Error, io};
 
-use tabled::{
-    builder::Builder,
-    settings::{object::Rows, Alignment, Modify, Style, Width},
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
-#[derive(Tabled)]
+// use itertools::Itertools;
+use ratatui::{prelude::*, widgets::*};
+use style::palette::tailwind;
+use unicode_width::UnicodeWidthStr;
+
 pub struct TableRow {
     pub id: String,
     query: String,
@@ -24,4 +30,259 @@ impl From<QStore> for Vec<TableRow> {
             })
             .collect()
     }
+}
+
+const PALETTES: [tailwind::Palette; 4] = [
+    tailwind::BLUE,
+    tailwind::EMERALD,
+    tailwind::INDIGO,
+    tailwind::RED,
+];
+
+const INFO_TEXT: &str =
+    "(Esc) quit | (↲) copy | (↑) move up | (↓) move down | (→) next color | (←) previous color";
+
+const ITEM_HEIGHT: usize = 4;
+
+struct TableColors {
+    buffer_bg: Color,
+    header_bg: Color,
+    header_fg: Color,
+    row_fg: Color,
+    selected_style_fg: Color,
+    normal_row_color: Color,
+    alt_row_color: Color,
+    footer_border_color: Color,
+}
+
+impl TableColors {
+    const fn new(color: &tailwind::Palette) -> Self {
+        Self {
+            buffer_bg: tailwind::SLATE.c950,
+            header_bg: color.c900,
+            header_fg: tailwind::SLATE.c200,
+            row_fg: tailwind::SLATE.c200,
+            selected_style_fg: color.c400,
+            normal_row_color: tailwind::SLATE.c950,
+            alt_row_color: tailwind::SLATE.c900,
+            footer_border_color: color.c400,
+        }
+    }
+}
+
+impl TableRow {
+    const fn ref_array(&self) -> [&String; 2] {
+        [&self.id, &self.query]
+    }
+
+    fn id(&self) -> &str {
+        &self.id
+    }
+
+    fn query(&self) -> &str {
+        &self.query
+    }
+}
+
+pub struct App {
+    state: TableState,
+    items: Vec<TableRow>,
+    longest_item_lens: (u16, u16), // order is (id, query)
+    scroll_state: ScrollbarState,
+    colors: TableColors,
+    color_index: usize,
+}
+
+impl App {
+    pub fn new(data: Vec<TableRow>) -> Self {
+        Self {
+            state: TableState::default().with_selected(0),
+            longest_item_lens: constraint_len_calculator(&data),
+            scroll_state: ScrollbarState::new((data.len() - 1) * ITEM_HEIGHT),
+            colors: TableColors::new(&PALETTES[0]),
+            color_index: 0,
+            items: data,
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+        self.scroll_state = self.scroll_state.position(i * ITEM_HEIGHT);
+    }
+
+    pub fn copy_to_clipboard(&mut self) {
+        let mut clipboard = Clipboard::new().unwrap();
+
+        if let Some(selected) = self.state.selected() {
+            let query = self.items[selected].query();
+            clipboard.set_text(query).unwrap();
+        }
+    }
+
+    pub fn next_color(&mut self) {
+        self.color_index = (self.color_index + 1) % PALETTES.len();
+    }
+
+    pub fn previous_color(&mut self) {
+        let count = PALETTES.len();
+        self.color_index = (self.color_index + count - 1) % count;
+    }
+
+    pub fn set_colors(&mut self) {
+        self.colors = TableColors::new(&PALETTES[self.color_index]);
+    }
+}
+
+pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                use KeyCode::*;
+                match key.code {
+                    Char('q') | Esc => return Ok(()),
+                    Char('j') | Down => app.next(),
+                    Char('k') | Up => app.previous(),
+                    Char('l') | Right => app.next_color(),
+                    Char('h') | Left => app.previous_color(),
+                    Char('c') | Enter => app.copy_to_clipboard(),
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn ui(f: &mut Frame, app: &mut App) {
+    let rects = Layout::vertical([Constraint::Min(5), Constraint::Length(3)]).split(f.size());
+
+    app.set_colors();
+    render_table(f, app, rects[0]);
+    render_scrollbar(f, app, rects[0]);
+    render_footer(f, app, rects[1]);
+}
+
+fn render_table(f: &mut Frame, app: &mut App, area: Rect) {
+    let header_style = Style::default()
+        .fg(app.colors.header_fg)
+        .bg(app.colors.header_bg);
+
+    let selected_style = Style::default()
+        .add_modifier(Modifier::REVERSED)
+        .fg(app.colors.selected_style_fg);
+
+    let header = ["Id", "Query"]
+        .into_iter()
+        .map(Cell::from)
+        .collect::<Row>()
+        .style(header_style)
+        .height(1);
+
+    let rows = app.items.iter().enumerate().map(|(i, data)| {
+        let color = match i % 2 {
+            0 => app.colors.normal_row_color,
+            _ => app.colors.alt_row_color,
+        };
+
+        let item = data.ref_array();
+
+        item.into_iter()
+            .map(|content| Cell::from(Text::from(format!("\n{content}\n"))))
+            .collect::<Row>()
+            .style(Style::new().fg(app.colors.row_fg).bg(color))
+            .height(4)
+    });
+
+    let bar = " █ ";
+
+    let t = Table::new(
+        rows,
+        [
+            Constraint::Min(app.longest_item_lens.0 + 1),
+            Constraint::Min(app.longest_item_lens.1 + 1),
+        ],
+    )
+    .header(header)
+    .highlight_style(selected_style)
+    .highlight_symbol(Text::from(vec![
+        "".into(),
+        bar.into(),
+        bar.into(),
+        "".into(),
+    ]))
+    .bg(app.colors.buffer_bg)
+    .highlight_spacing(HighlightSpacing::Always);
+    f.render_stateful_widget(t, area, &mut app.state);
+}
+
+fn constraint_len_calculator(items: &[TableRow]) -> (u16, u16) {
+    let id_len = items
+        .iter()
+        .map(TableRow::id)
+        .map(UnicodeWidthStr::width)
+        .max()
+        .unwrap_or(0);
+
+    let query_len = items
+        .iter()
+        .map(TableRow::query)
+        .flat_map(str::lines)
+        .map(UnicodeWidthStr::width)
+        .max()
+        .unwrap_or(0);
+
+    #[allow(clippy::cast_possible_truncation)]
+    (id_len as u16, query_len as u16)
+}
+
+fn render_scrollbar(f: &mut Frame, app: &mut App, area: Rect) {
+    f.render_stateful_widget(
+        Scrollbar::default()
+            .orientation(ScrollbarOrientation::VerticalRight)
+            .begin_symbol(None)
+            .end_symbol(None),
+        area.inner(&Margin {
+            vertical: 1,
+            horizontal: 1,
+        }),
+        &mut app.scroll_state,
+    );
+}
+
+fn render_footer(f: &mut Frame, app: &App, area: Rect) {
+    let info_footer = Paragraph::new(Line::from(INFO_TEXT))
+        .style(Style::new().fg(app.colors.row_fg).bg(app.colors.buffer_bg))
+        .centered()
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .border_style(Style::new().fg(app.colors.footer_border_color))
+                .border_type(BorderType::Double),
+        );
+    f.render_widget(info_footer, area);
 }
